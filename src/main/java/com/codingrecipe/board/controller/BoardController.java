86546c8422c14b118b95e54cde925b1d6134fc4b
taejong12 <dbxowhdsla12@naver.com>
2023-03-23 10:28:18 +0900

    5.게시글 조회

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,17 @@ public class BoardController {
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
         return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        // 1. 해당 게시글의 조회수를 하나 올리고
+        // 2. 게시글 데이터를 가져와서 detail.html에 출력
+
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
     }
 
 
--- src/main/java/com/codingrecipe/board/service/BoardService.java
+++ src/main/java/com/codingrecipe/board/service/BoardService.java
@@ -3,11 +3,13 @@ package com.codingrecipe.board.service;
 import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.entity.BoardEntity;
 import com.codingrecipe.board.repository.BoardRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 // DTO -> Entity 로 변환(controller -> DB) (Entity Class)
@@ -31,5 +33,22 @@ public class BoardService {
             boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
         }
         return boardDTOList;
+    }
+
+//    추가된 메서드 사용시 트랜잭션 어노테이션 써야 한다
+    @Transactional
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if(optionalBoardEntity.isPresent()){
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDTO boardDTO = BoardDTO.toBoardDTO(boardEntity);
+            return boardDTO;
+        } else {
+            return null;
+        }
     }
 }
