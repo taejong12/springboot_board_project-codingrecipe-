c7b0aa1c399f16041396b1d545916b47ccd62b6c
taejong12 <dbxowhdsla12@naver.com>
2023-03-23 10:10:05 +0900

    4.게시글 목록

--- src/main/java/com/codingrecipe/board/service/BoardService.java
+++ src/main/java/com/codingrecipe/board/service/BoardService.java
@@ -6,6 +6,9 @@ import com.codingrecipe.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // DTO -> Entity 로 변환(controller -> DB) (Entity Class)
 // Entity -> DTO 로 변환(DB -> controller) (DTO Class)
@@ -19,5 +22,14 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for(BoardEntity boardEntity : boardEntityList){
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
